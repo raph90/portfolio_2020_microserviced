1b051a9406923e09453da6828a68ab5515c58a51
Raphael Hetherington <raph37heth@gmail.com>
2020-04-18 19:38:38 +0100

    aded submodules?

---
+++ gatsby_blog/src/pages/404.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <a href="/todos">MAYBE YOU WANT TO DO SOME TODOS???</a>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Portfolio 2020
+
+## Aims
+
+- To create a microservice architecture for my personal portfolio
+- The portfolio site itself will be an SSR gatsby site hosted via an NGINX container
+- it will connect to the various applications that I build in the microservices architecture
+
+
+## Currently Working on:
+- initial build and setup
